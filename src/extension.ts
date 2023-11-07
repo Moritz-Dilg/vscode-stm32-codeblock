30040d2302d573764357fce62adf229fcb81b13f
Moritz-Dilg <moritz@dilg-online.de>
2023-11-07 09:58:15 +0100

    Print file content on change

--- src/extension.ts
+++ src/extension.ts
@@ -6,21 +6,20 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "stm32-codeblock" is now active!');
+	// Update codeBlock when text changes
+	const changeTextDisposable = vscode.workspace.onDidChangeTextDocument((event) => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) return;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('stm32-codeblock.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from STM32 CodeBlock!');
+		const document = event.document;
+		if (document.languageId !== 'c' && document.languageId !== 'cpp') return;
+
+		const documentContent = document.getText();
+		console.log(documentContent);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(changeTextDisposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
